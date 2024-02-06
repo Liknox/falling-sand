bad9840d2ee4b35cd09011902d058a0cfa0d2dfd
Nazar Koval <n.kovaldev@gmail.com>
2024-02-06 03:55:36 +0200

    chore: add withIn functions

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ let cols, rows
 let hueValue = 200
 
 let gravity = 0.1
+
+function withInCols(i) {
+	return i >= 0 && i <= cols - 1
+}
+
+function withInRows(j) {
+	return j >= 0 && j <= rows - 1
+}
+
