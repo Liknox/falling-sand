9f95ab168cea640e4ad6dc72c4b376a99eba43de
Nazar Koval <n.kovaldev@gmail.com>
2024-02-06 03:57:30 +0200

    feat: add 2D array transofrmation

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import "./style.css"
 
+function make2DArray(cols, rows) {
+	let arr = new Array(cols)
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = new Array(rows)
+		for (let j = 0; j < arr[i].length; j++) {
+			arr[i][j] = 0
+		}
+	}
+	return arr
+}
+
 let grid
 let velocityGrid
 
@@ -17,3 +28,13 @@ function withInRows(j) {
 	return j >= 0 && j <= rows - 1
 }
 
+function setup() {
+	createCanvas(800, 800)
+	colorMode(HSB, 360, 255, 255)
+	cols = width / w
+	rows = height / w
+	grid = make2DArray(cols, rows)
+	velocityGrid = make2DArray(cols, rows, 1)
+}
+
+function mouseDragged() {}
